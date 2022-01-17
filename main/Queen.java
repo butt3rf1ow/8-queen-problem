271e7eaefb9725c1f68d9c1568b62dea0f491929
Famil <81039144+f-moon@users.noreply.github.com>
2022-01-18 02:56:36 +0400

    Delete main directory

--- main/Queen.java
+++
@@ -1,9 +0,0 @@
-public class Queen extends Piece{
-    Queen(String name, Board world, Coord positiion){
-        super(name, world, positiion);
-    }
-    
-    Queen(String name, Board world, int row, int column){
-        super(name, world, row, column);
-    }
-}
